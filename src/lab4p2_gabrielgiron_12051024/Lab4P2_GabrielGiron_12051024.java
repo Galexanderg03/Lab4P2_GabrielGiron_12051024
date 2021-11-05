5fad48cb2ba000fa9f89553a69aa685d3b94420e
Galexanderg03 <galexanderg03@gmail.com>
2021-11-05 16:11:32 -0600

    Subiendo Archivos

---
+++ src/lab4p2_gabrielgiron_12051024/Lab4P2_GabrielGiron_12051024.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_gabrielgiron_12051024;
+
+/**
+ *
+ * @author Galex
+ */
+public class Lab4P2_GabrielGiron_12051024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
